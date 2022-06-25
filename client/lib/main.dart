93b512743f57e8f1720066e021f7537ea74668a0
isaikin_va <v.chaser@mail.ru>
2022-06-25 14:49:46 +1000

    clean trash

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,55 +1,19 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hvac_remote_client/application/application.dart';
-import 'package:path_provider/path_provider.dart';
 
-import 'mqtt_lts_test.dart';
-
-bool setCrashlytics = false;
-
-main() => runApp(const _MyApp());
-
-class _MyApp extends StatelessWidget {
-  const _MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Center(
-            child: TextButton(
-              onPressed: mainCb,
-              child: Text('do'),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-void mainCb() async {
-  final mqtt = MqttDb();
-  await mqtt.connect();
-}
-
-void _main() {
+void main() {
   runZonedGuarded<Future<void>>(
     () async {
       await _setRunAppPresets();
       runApp(const Application());
     },
     (e, s) {
-      if (setCrashlytics) {
-        FirebaseCrashlytics.instance.recordError(e, s, printDetails: true);
-      }
+      // FirebaseCrashlytics.instance.recordError(e, s, printDetails: true);
     },
   );
 }
@@ -62,6 +26,4 @@ Future<void> _setRunAppPresets() async {
 
   //changed on some pages
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-
-  setCrashlytics = true;
 }
