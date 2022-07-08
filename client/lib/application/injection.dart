a9945a46a705669b91ffaf7218affc1b76e850e7
isaikin_va <v.chaser@mail.ru>
2022-07-08 12:59:19 +1000

    hot fixes

--- client/lib/application/injection.dart
+++ client/lib/application/injection.dart
@@ -1,11 +1,17 @@
 import 'package:get_it/get_it.dart';
 import 'package:hvac_remote_client/data/repository/role/firebase.dart';
+import 'package:hvac_remote_client/data/service/auth/firebase.dart';
 import 'package:hvac_remote_client/domain/repo/role.dart';
+import 'package:hvac_remote_client/domain/service/auth.dart';
 
-final GetIt getIt = GetIt.instance; //service locator
+final GetIt getIt = GetIt.instance;
 
 void inject() {
   getIt.registerSingleton<RoleRepo>(
     FirebaseRoleRepo(),
+  );
+
+  getIt.registerSingleton<AuthService>(
+    FirebaseAuthService(),
   );
 }
